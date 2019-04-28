14259bc924b6d4b1c1b1950362f3b946a68b6404
miguelpduarte <miguelpduarte98@gmail.com>
2019-04-28 13:52:41 +0100

    Applied refactor by pulling up the parameter and local variables code generation to superclass (MethodDeclarationNode)

--- MethodDeclarationNode.java
+++ MethodDeclarationNode.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public abstract class MethodDeclarationNode extends NodeWithSymbolTable implements DeclarationNode {
   // protected String name; // Declared in superclass for access
@@ -32,6 +34,24 @@ public abstract class MethodDeclarationNode extends NodeWithSymbolTable implemen
                                     .toArray(VariableType[]::new);
 
     return SymbolTableScopes.calculateMethodIdentifier(identifier, params_types);
+  }
+
+  protected final void generateParametersAndLocalsCode(StringBuilder sb) {
+    // Temporary placeholder
+    sb.append("\t.limit stack 10 ; temporary while stack size is not being calculated\n");
+    // Number of local variables = number of entries in the symbol table + 1
+    sb.append(String.format("\t.limit locals %d\n", this.calcLocalsLimit() + 1));
+
+    // Creating the local variables (done here instead of in the child nodes because of the sequential indexes)
+
+    // The starting index is the number of parameters (+1 because of 'this')
+    int locals_start_idx = ((int) this.symbol_table.values().stream().filter(Variable::isParameter).count()) + 1;
+    List<Variable> local_variables = this.symbol_table.values().stream().filter(Predicate.not(Variable::isParameter)).collect(Collectors.toList());
+
+    for (Variable local_var : local_variables) {
+      local_var.setLocalVarIndex(locals_start_idx);
+      sb.append(String.format(local_var.toJasminDeclaration(), locals_start_idx++));
+    }
   }
 }
 /* JavaCC - OriginalChecksum=3d0bb6bccafce97c55dc7c526183eb43 (do not edit this line) */
